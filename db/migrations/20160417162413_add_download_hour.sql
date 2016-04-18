66c0b7cc05092e11a5394947ac90f5d848933662
Eric Florenzano <floguy@gmail.com>
2016-04-17 17:27:37 -0700

    Add an index

--- db/migrations/20160417162413_add_download_hour.sql
+++ db/migrations/20160417162413_add_download_hour.sql
@@ -10,7 +10,9 @@ CREATE TABLE download_hour (
     FOREIGN KEY (user_id) REFERENCES auth_user(id),
     UNIQUE(file_id, hour, ip)
 );
+CREATE INDEX download_hour_file_id_idx ON download_hour (file_id);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
+DROP INDEX download_hour_file_id_idx;
 DROP TABLE download_hour;
